52e63fc9a805ba2ded8f5c6af614da0fbe53697b
Lekxpa <LekxpaP@gmail.com>
2023-07-03 00:20:47 +0300

    add task04

--- HW_04/Task04.py
+++ HW_04/Task04.py
@@ -5,69 +5,73 @@ import sys
 
 START_SUM = 0
 START_OPERATIONS = 1
-RICH_VALUE = 5_000_000
-RICH_NALOG = 10
-MULT = 50
-MAX_NALOG = 600
-MIN_NALOG = 30
+SUM_OF_WEALTH = 5_000_000
+NALOG_OF_WEALTH = 10
+MULTIPL = 50
+MAX_CASH = 600
+MIN_CASH = 30
+INTEREST_FOR_CASH = 0.015
+COUNT_OF_OPERATIONS = 3
+INTEREST_FOR_OPERATIONS = 0.03
 
-
-def check_rich(bal, op, op_list):
-    if bal > RICH_VALUE:
-        nalog = (bal - RICH_VALUE) // RICH_NALOG
-        op_list.append((op, 'Налог на богатство', nalog))
-        bal -= nalog
-    return bal
 
 def check_input(message):
     while True:
         inp = int(input(message))
-        if inp % MULT != 0:
-            print("Недопустимая сумма. Сумма должна быть кратна 50!")
+        if inp % MULTIPL != 0:
+            print("Сумма должна быть кратна 50!")
         else:
             return inp
+        
 
-
-def increase(bal, op, op_list):
-    inc = check_input("Введите количество для пополнения: ")
-    if op % 3 == 0:
-        bal += bal * 0.03
+def increase(bal, op, ls_oper):
+    inc = check_input("Введите сумму пополнения: ")
+    if op % COUNT_OF_OPERATIONS == 0:
+        bal += bal * INTEREST_FOR_OPERATIONS
     bal += inc
-    op_list.append((op, 'пополнение', inc))
+    ls_oper.append(op, 'пополнение', inc)
     op += 1
-    bal = check_rich(bal, op, op_list)
+    bal = check_rich(bal, op, ls_oper)
     return bal, op
 
 
-def decrease(bal, op, op_list):
-    bal = check_rich(bal, op, op_list)
-    dec = check_input("Введите количество для снятия: ")
-    percent = dec * 0.015
-    if percent < MIN_NALOG:
-        percent = MIN_NALOG
-    elif percent > MAX_NALOG:
-        percent = MAX_NALOG
-    dec_perc = dec + percent
-    if bal > dec_perc:
-        bal -= dec_perc
+def decrease(bal, op, ls_oper):
+    bal = check_rich(bal, op, ls_oper)
+    dec = check_input('Введите сумму снятия: ')
+    percent = dec * INTEREST_FOR_CASH
+    if percent < MIN_CASH:
+        percent = MIN_CASH
+    elif percent > MAX_CASH:
+        percent = MAX_CASH
+    dec_percent = dec + percent
+    if bal > dec_percent:
+        bal -= dec_percent
     else:
         print("Недостаточно средств!")
-    if op % 3 == 0:
-        bal += bal * 0.03
-    op_list.append((op, 'Снятие', dec))
-    op_list.append((op, 'Комиссия за снятие', percent))
+
+    if op % COUNT_OF_OPERATIONS == 0:
+        bal += bal * INTEREST_FOR_OPERATIONS
+    ls_oper.append((op, 'Снятие', dec))
+    ls_oper.append((op, 'Комиссия за снятие: '))
     op += 1
     return bal, op
 
+def check_rich(bal, op, ls_oper):
+    if bal > SUM_OF_WEALTH:
+        nalog = (bal - SUM_OF_WEALTH) // NALOG_OF_WEALTH
+        ls_oper.append((op, 'Налог на богатство', nalog))
+        bal -= nalog
+    return bal
 
 def start():
     balance = START_SUM
     operations = START_OPERATIONS
-    operations_list = []
+    lst = []
     while True:
         select = int(input(f"""Баланс: {balance}
-Операции со счётом: {operations - 1}
 
+Операции со счётом: {operations - 1}
+    
 Доступные действия:
 1. Пополнить
 2. Снять
@@ -76,14 +80,13 @@ def start():
 Выберите действие: """))
         match select:
             case 1:
-                balance, operations = increase(balance, operations, operations_list)
+                balance, operations = increase(balance, operations, lst)
             case 2:
-                balance, operations = decrease(balance, operations, operations_list)
+                balance, operations = decrease(balance, operations, lst)
             case 3:
-                print('Выполненные операции:', *operations_list, sep="\n")
+                print('Все операции: ', *lst, sep='\n')
                 sys.exit()
             case _:
                 print("Повторите попытку")
-
 
 start()
