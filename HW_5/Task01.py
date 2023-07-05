edd1ff6d659a8e99de4c88917b81da2268a073fc
Lekxpa <LekxpaP@gmail.com>
2023-07-05 23:24:32 +0300

    correct task01 in HW_5

--- HW_5/Task01.py
+++ HW_5/Task01.py
@@ -2,10 +2,12 @@
 # Функция возвращает кортеж из трёх элементов: путь, имя файла, расширение файла.
 
 def file_info(file_path):
-    *file_path, f_name, f_roat = file_path.replace('.', '\\').split('\\')
-    file_path = '\\'.join(file_path)
-    return (f'\nПуть - {path}\nИмя файла - {f_name}\nРасширение файла - {f_roat}\n')
+    *file_path, f_name, f_roat = file_path.replace('.', '/').split('/')
+    file_path = '/'.join(file_path)
+    return file_path, f_name, f_roat
+    # return (f'\nПуть - {file_path}\nИмя файла - {f_name}\nРасширение файла - {f_roat}\n')
 
-path = 'D:\\Linux\\ulin\\Logs\\vbox.txt'
+path = 'D:/Linux/ulin/Logs/vbox.txt'
 
-print(file_info(path))
+print('Путь - {}, Имя файла - {}, Расширение файла -  {}'.format(*file_info(path)))
+# print(file_info(path))
